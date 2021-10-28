627308b52f8b16dee0baaf03db211002b1ccd74e
zuzuddoo <67114919+zuzudoo@users.noreply.github.com>
2021-10-28 19:16:25 +0800

    Update getters.js

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,7 +3,12 @@ export default {
   cartLength(state) {
     return state.cartList.length
   },
+  // 2.返回购物车所有内容
   cartList(state) {
     return state.cartList
+  },
+  // 3.返回分类选中的item
+  clickName(state) {
+    return state.clickName
   }
 }
